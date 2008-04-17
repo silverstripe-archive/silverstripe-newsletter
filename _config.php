8bd8049f26b2a4d2af09f99153eeb5cadc1a08c1
Ingo Schommer <ingo@silverstripe.com>
2008-04-17 10:34:37 +0000

    API CHANGE Moving newsletter functionality to its own module

---
+++ _config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * URL rules for the CMS module
+ * 
+ * @package cms
+ */
+Director::addRules(50, array(
+	'admin/newsletter/$Action/$ID' => 'NewsletterAdmin',
+	'unsubscribe/$Email/$MailingList' => 'Unsubscribe_Controller'
+));
+
+LeftAndMain::add_menu_item(
+	"newsletter", 
+	_t('LeftAndMain.NEWSLETTERS',"Newsletters",PR_HIGH,"Menu title"),
+	"admin/newsletter/", 
+	"NewsletterAdmin"
+);
+
+?>

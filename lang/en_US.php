1f2e4d25c9a2691b305719de32cceb4cd4606040
Ingo Schommer <ingo@silverstripe.com>
2008-11-01 15:29:19 +0000

    MINOR Collecting entities for language master table with new i18nTextCollector functionality. The table is now sorted alphabetically by namespace and entity. Entities now include more translatable statics from DataObject subclasses like $db, $has_one etc.

--- lang/en_US.php
+++ lang/en_US.php
@@ -3,88 +3,106 @@
 global $lang;
 
 $lang['en_US']['BatchProcess_Controller']['ERROR'] = 'ERROR: Could not continue process';
-$lang['en_US']['Newsletter']['NEWSLETTER'] = 'Newsletter';
-$lang['en_US']['Newsletter']['SUBJECT'] = 'Subject';
+$lang['en_US']['Member']['EMAILPASSWORDAPPENDIX'] = 'Your password has been changed. Please keep this email, for future reference.';
+$lang['en_US']['Member']['EMAILPASSWORDINTRO'] = 'Here\'s your new password';
 $lang['en_US']['Newsletter']['CONTENT'] = 'Content';
-$lang['en_US']['Newsletter']['SENTREPORT'] = 'Sent Status Report';
+$lang['en_US']['Newsletter']['NEWSLETTER'] = 'Newsletter';
+$lang['en_US']['Newsletter']['PLURALNAME'] = array(
+	'Newsletters',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
 $lang['en_US']['Newsletter']['SENTAT'] = 'Sent at';
-$lang['en_US']['NewsletterAdmin']['NLSETTINGS'] = 'Newsletter Settings';
-$lang['en_US']['NewsletterAdmin']['NEWSLTYPE'] = 'Newsletter Type';
+$lang['en_US']['Newsletter']['SENTREPORT'] = 'Sent Status Report';
+$lang['en_US']['Newsletter']['SINGULARNAME'] = array(
+	'Newsletter',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['Newsletter']['SUBJECT'] = 'Subject';
+$lang['en_US']['Newsletter']['db_Content'] = array(
+	'Content',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Newsletter']['db_SentDate'] = array(
+	'SentDate',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Newsletter']['db_Status'] = array(
+	'Status',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Newsletter']['db_Subject'] = array(
+	'Subject',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['Newsletter']['has_many_Recipients'] = array(
+	'Recipients',
+	50,
+	'Name of an object relation, mainly used for automatically generating forms'
+);
+$lang['en_US']['Newsletter']['has_many_SentRecipients'] = array(
+	'SentRecipients',
+	50,
+	'Name of an object relation, mainly used for automatically generating forms'
+);
+$lang['en_US']['NewsletterAdmin']['ADDEDTOBL'] = 'was added to blacklist';
+$lang['en_US']['NewsletterAdmin']['BOUNCED'] = 'Bounced';
 $lang['en_US']['NewsletterAdmin']['FROMEM'] = 'From email address';
-$lang['en_US']['NewsletterAdmin']['TEMPLATE'] = 'Template';
-$lang['en_US']['NewsletterAdmin']['SAVE'] = 'Save';
-$lang['en_US']['NewsletterAdmin']['RECIPIENTS'] = 'Recipients';
 $lang['en_US']['NewsletterAdmin']['IMPORT'] = 'Import';
 $lang['en_US']['NewsletterAdmin']['IMPORTFROM'] = 'Import from file';
-$lang['en_US']['NewsletterAdmin']['UNSUBSCRIBERS'] = 'Unsubscribers';
-$lang['en_US']['NewsletterAdmin']['BOUNCED'] = 'Bounced';
-$lang['en_US']['NewsletterAdmin']['REMOVEDSUCCESS'] = 'was removed from the mailing list';
-$lang['en_US']['NewsletterAdmin']['RESEND'] = 'Resend';
-$lang['en_US']['NewsletterAdmin']['SEND'] = 'Send...';
-$lang['en_US']['NewsletterAdmin']['SENDING'] = 'Sending emails...';
-$lang['en_US']['NewsletterAdmin']['NONLSPECIFIED'] = 'No newsletter specified';
-$lang['en_US']['NewsletterAdmin']['SENTTESTTO'] = 'Sent test to ';
-$lang['en_US']['NewsletterAdmin']['PLEASEENTERMAIL'] = 'Please enter an email address';
-$lang['en_US']['NewsletterAdmin']['SAVED'] = 'Saved';
-$lang['en_US']['NewsletterAdmin']['ADDEDTOBL'] = 'was added to blacklist';
-$lang['en_US']['NewsletterAdmin']['REMOVEDFROMBL'] = 'was removed from blacklist';
+$lang['en_US']['NewsletterAdmin']['MEWDRAFTMEWSL'] = 'New draft newsletter';
 $lang['en_US']['NewsletterAdmin']['NEWLIST'] = 'New mailing list';
 $lang['en_US']['NewsletterAdmin']['NEWNEWSLTYPE'] = 'New newsletter type';
-$lang['en_US']['NewsletterAdmin']['MEWDRAFTMEWSL'] = 'New draft newsletter';
+$lang['en_US']['NewsletterAdmin']['NEWSLTYPE'] = 'Newsletter Type';
+$lang['en_US']['NewsletterAdmin']['NLSETTINGS'] = 'Newsletter Settings';
+$lang['en_US']['NewsletterAdmin']['NONLSPECIFIED'] = 'No newsletter specified';
+$lang['en_US']['NewsletterAdmin']['PLEASEENTERMAIL'] = 'Please enter an email address';
+$lang['en_US']['NewsletterAdmin']['RECIPIENTS'] = 'Recipients';
+$lang['en_US']['NewsletterAdmin']['REMOVEDFROMBL'] = 'was removed from blacklist';
+$lang['en_US']['NewsletterAdmin']['REMOVEDSUCCESS'] = 'was removed from the mailing list';
+$lang['en_US']['NewsletterAdmin']['RESEND'] = 'Resend';
+$lang['en_US']['NewsletterAdmin']['SAVE'] = 'Save';
+$lang['en_US']['NewsletterAdmin']['SAVED'] = 'Saved';
+$lang['en_US']['NewsletterAdmin']['SEND'] = 'Send...';
+$lang['en_US']['NewsletterAdmin']['SENDING'] = 'Sending emails...';
+$lang['en_US']['NewsletterAdmin']['SENTTESTTO'] = 'Sent test to ';
 $lang['en_US']['NewsletterAdmin']['SHOWCONTENTS'] = 'Show contents';
-$lang['en_US']['NewsletterType']['MAILINGLIST'] = 'Mailing List:';
-$lang['en_US']['NewsletterType']['NEWSLETTERTYPE'] = 'Newsletter Type';
-$lang['en_US']['NewsletterType']['SENDFROM'] = 'Send newsletters from';
-$lang['en_US']['NewsletterType']['DRAFTS'] = 'Drafts';
-$lang['en_US']['NewsletterType']['DRAFT'] = 'Draft';
-$lang['en_US']['NewsletterType']['SENT'] = 'Sent';
-$lang['en_US']['NewsletterType']['SEND'] = 'Send';
-$lang['en_US']['NewsletterType']['UNSUBSCRIBED'] = 'Unsubscribed';
-$lang['en_US']['NewsletterType']['BOUNCED'] = 'Bounced';
-$lang['en_US']['NewsletterType']['RECIPIENTS'] = 'Recipients';
-$lang['en_US']['NewsletterType']['IMPORT'] = 'Import';
-$lang['en_US']['NewsletterType']['IMPORTFROM'] = 'Import from file';
-$lang['en_US']['NewsletterType']['TEMPLATE'] = 'Template';
-$lang['en_US']['TemplateList']['NONE'] = 'None';
-$lang['en_US']['Unsubscribe']['SUCCESS'] = 'Thank you. You have been removed from the selected groups';
-$lang['en_US']['Unsubscribe']['NOMLSELECTED'] = 'You need to select at least one mailing list to unsubscribe from.';
-$lang['en_US']['Unsubcribe']['SUBSCRIBEDTO'] = 'You are subscribed to the following lists:';
-$lang['en_US']['Unsubscribe']['UNSUBSCRIBE'] = 'Unsubscribe';
-$lang['en_US']['Unsubscribe']['NOTSUBSCRIBED'] = 'I\'m sorry, but %s doesn\'t appear to be in any of our mailing lists.';
-$lang['en_US']['Unsubscribe']['EMAILADDR'] = 'Email address';
-$lang['en_US']['Unsubscribe']['SHOWLISTS'] = 'Show lists';
-$lang['en_US']['Unsubscribe']['REMOVESUCCESS'] = 'Thank you. %s will no longer receive the %s.';
-$lang['en_US']['Member']['EMAILPASSWORDINTRO'] = 'Here\'s your new password';
-$lang['en_US']['Member']['EMAILPASSWORDAPPENDIX'] = 'Your password has been changed. Please keep this email, for future reference.';
+$lang['en_US']['NewsletterAdmin']['TEMPLATE'] = 'Template';
+$lang['en_US']['NewsletterAdmin']['UNSUBSCRIBERS'] = 'Unsubscribers';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['BLACKLISTED'] = 'Blacklisted';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['DATE'] = 'Date';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['EMADD'] = 'Email address';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['HAVEBOUNCED'] = 'Emails that have bounced';
 $lang['en_US']['NewsletterAdmin_BouncedList.ss']['INSTRUCTIONS'] = 'Instructions:';
 $lang['en_US']['NewsletterAdmin_BouncedList.ss']['INSTRUCTIONS1'] = 'Uncheck the box to enable sending to a blacklisted email address.';
 $lang['en_US']['NewsletterAdmin_BouncedList.ss']['INSTRUCTIONS2'] = 'To remove a recipients\'s email address from your mailing list, click the icon';
-$lang['en_US']['NewsletterAdmin_BouncedList.ss']['HAVEBOUNCED'] = 'Emails that have bounced';
-$lang['en_US']['NewsletterAdmin_BouncedList.ss']['BLACKLISTED'] = 'Blacklisted';
-$lang['en_US']['NewsletterAdmin_BouncedList.ss']['UNAME'] = 'User name';
-$lang['en_US']['NewsletterAdmin_BouncedList.ss']['EMADD'] = 'Email address';
-$lang['en_US']['NewsletterAdmin_BouncedList.ss']['RESON'] = 'Reason:';
-$lang['en_US']['NewsletterAdmin_BouncedList.ss']['DATE'] = 'Date';
 $lang['en_US']['NewsletterAdmin_BouncedList.ss']['NOBOUNCED'] = 'No emails sent have bounced.';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['RESON'] = 'Reason:';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['UNAME'] = 'User name';
 $lang['en_US']['NewsletterAdmin_SiteTree.ss']['DRAFTS'] = 'Drafts';
-$lang['en_US']['NewsletterAdmin_SiteTree.ss']['SENT'] = 'Sent Items';
 $lang['en_US']['NewsletterAdmin_SiteTree.ss']['MAILLIST'] = 'Mailing List';
+$lang['en_US']['NewsletterAdmin_SiteTree.ss']['SENT'] = 'Sent Items';
+$lang['en_US']['NewsletterAdmin_UnsubscribedList.ss']['NOUNSUB'] = 'No users have unsubscribed from this newsletter.';
 $lang['en_US']['NewsletterAdmin_UnsubscribedList.ss']['UNAME'] = 'User name';
 $lang['en_US']['NewsletterAdmin_UnsubscribedList.ss']['UNSUBON'] = 'Unsubscribed on';
-$lang['en_US']['NewsletterAdmin_UnsubscribedList.ss']['NOUNSUB'] = 'No users have unsubscribed from this newsletter.';
-$lang['en_US']['NewsletterAdmin_left.ss']['NEWSLETTERS'] = 'Newsletters';
+$lang['en_US']['NewsletterAdmin_left.ss']['ADDDRAFT'] = 'Add new draft';
+$lang['en_US']['NewsletterAdmin_left.ss']['ADDTYPE'] = 'Add new type';
 $lang['en_US']['NewsletterAdmin_left.ss']['CREATE'] = 'Create';
 $lang['en_US']['NewsletterAdmin_left.ss']['DEL'] = 'Delete';
-$lang['en_US']['NewsletterAdmin_left.ss']['ADDTYPE'] = 'Add new type';
-$lang['en_US']['NewsletterAdmin_left.ss']['ADDDRAFT'] = 'Add new draft';
-$lang['en_US']['NewsletterAdmin_left.ss']['GO'] = 'Go';
-$lang['en_US']['NewsletterAdmin_left.ss']['SELECTDRAFTS'] = 'Select the drafts that you want to delete and then click the button below';
 $lang['en_US']['NewsletterAdmin_left.ss']['DELETEDRAFTS'] = 'Delete the selected drafts';
-$lang['en_US']['NewsletterAdmin_right.ss']['SENDTEST'] = 'Send test to';
+$lang['en_US']['NewsletterAdmin_left.ss']['GO'] = 'Go';
+$lang['en_US']['NewsletterAdmin_left.ss']['NEWSLETTERS'] = 'Newsletters';
+$lang['en_US']['NewsletterAdmin_left.ss']['SELECTDRAFTS'] = 'Select the drafts that you want to delete and then click the button below';
+$lang['en_US']['NewsletterAdmin_right.ss']['CANCEL'] = 'Cancel';
 $lang['en_US']['NewsletterAdmin_right.ss']['ENTIRE'] = 'Send to the entire mailing list';
 $lang['en_US']['NewsletterAdmin_right.ss']['ONLYNOT'] = 'Send to only people not previously sent to';
-$lang['en_US']['NewsletterAdmin_right.ss']['CANCEL'] = 'Cancel';
 $lang['en_US']['NewsletterAdmin_right.ss']['SEND'] = 'Send newsletter';
+$lang['en_US']['NewsletterAdmin_right.ss']['SENDTEST'] = 'Send test to';
 $lang['en_US']['NewsletterAdmin_right.ss']['WELCOME1'] = array(
 	'Welcome to the',
 	50,
@@ -97,28 +115,125 @@ $lang['en_US']['NewsletterAdmin_right.ss']['WELCOME2'] = array(
 $lang['en_US']['NewsletterList.ss']['CHOOSEDRAFT1'] = 'Please choose a draft on the left, or';
 $lang['en_US']['NewsletterList.ss']['CHOOSEDRAFT2'] = 'add one';
 $lang['en_US']['NewsletterList.ss']['CHOOSESENT'] = 'Please choose a sent item on the left.';
+$lang['en_US']['NewsletterType']['BOUNCED'] = 'Bounced';
+$lang['en_US']['NewsletterType']['DRAFT'] = 'Draft';
+$lang['en_US']['NewsletterType']['DRAFTS'] = 'Drafts';
+$lang['en_US']['NewsletterType']['IMPORT'] = 'Import';
+$lang['en_US']['NewsletterType']['IMPORTFROM'] = 'Import from file';
+$lang['en_US']['NewsletterType']['MAILINGLIST'] = 'Mailing List:';
+$lang['en_US']['NewsletterType']['NEWSLETTERTYPE'] = 'Newsletter Type';
+$lang['en_US']['NewsletterType']['PLURALNAME'] = array(
+	'Newsletter Typs',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['NewsletterType']['RECIPIENTS'] = 'Recipients';
+$lang['en_US']['NewsletterType']['SEND'] = 'Send';
+$lang['en_US']['NewsletterType']['SENDFROM'] = 'Send newsletters from';
+$lang['en_US']['NewsletterType']['SENT'] = 'Sent';
+$lang['en_US']['NewsletterType']['SINGULARNAME'] = array(
+	'Newsletter Type',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['NewsletterType']['TEMPLATE'] = 'Template';
+$lang['en_US']['NewsletterType']['UNSUBSCRIBED'] = 'Unsubscribed';
+$lang['en_US']['NewsletterType']['db_FromEmail'] = array(
+	'FromEmail',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['NewsletterType']['db_Sent'] = array(
+	'Sent',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['NewsletterType']['db_Template'] = array(
+	'Template',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['NewsletterType']['db_Title'] = array(
+	'Title',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['NewsletterType']['has_many_Newsletters'] = array(
+	'Newsletters',
+	50,
+	'Name of an object relation, mainly used for automatically generating forms'
+);
+$lang['en_US']['Newsletter_RecipientImportField.ss']['CHANGED'] = 'Number of details changed:';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['IMPORTED'] = 'New members imported:';
 $lang['en_US']['Newsletter_RecipientImportField.ss']['IMPORTNEW'] = 'Imported new members';
 $lang['en_US']['Newsletter_RecipientImportField.ss']['MLRELOAD1'] = 'To see the new members on the Recipients tab, you need to';
 $lang['en_US']['Newsletter_RecipientImportField.ss']['MLRELOAD2'] = 'reload the Mailing List';
-$lang['en_US']['Newsletter_RecipientImportField.ss']['IMPORTED'] = 'New members imported:';
-$lang['en_US']['Newsletter_RecipientImportField.ss']['UPDATED'] = 'Members updated:';
-$lang['en_US']['Newsletter_RecipientImportField.ss']['CHANGED'] = 'Number of details changed:';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['SEC'] = 'seconds';
 $lang['en_US']['Newsletter_RecipientImportField.ss']['SKIPPED'] = 'Records skipped:';
 $lang['en_US']['Newsletter_RecipientImportField.ss']['TIME'] = 'Time taken:';
-$lang['en_US']['Newsletter_RecipientImportField.ss']['SEC'] = 'seconds';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['UPDATED'] = 'Members updated:';
 $lang['en_US']['Newsletter_RecipientImportField_Table.ss']['CONTENTSOF'] = 'Contents of';
-$lang['en_US']['Newsletter_RecipientImportField_Table.ss']['YES'] = 'Confirm';
 $lang['en_US']['Newsletter_RecipientImportField_Table.ss']['NO'] = 'Cancel';
 $lang['en_US']['Newsletter_RecipientImportField_Table.ss']['RECIMPORTED'] = 'Recipients imported from';
-$lang['en_US']['Newsletter_SentStatusReport.ss']['SENDFAIL'] = 'Sending to the Following Recipients Failed';
-$lang['en_US']['Newsletter_SentStatusReport.ss']['EMAIL'] = 'Email';
+$lang['en_US']['Newsletter_RecipientImportField_Table.ss']['YES'] = 'Confirm';
 $lang['en_US']['Newsletter_SentStatusReport.ss']['DATE'] = 'Date';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['EMAIL'] = 'Email';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['FAILEDBL'] = 'Sending to the Following Recipients Did Not Occur Because They Are BlackListed';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['FN'] = 'Firstname';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['NEWSNEVERSENT'] = 'The Newsletter has Never Been Sent to Following Subscribers';
 $lang['en_US']['Newsletter_SentStatusReport.ss']['RES'] = 'Result';
 $lang['en_US']['Newsletter_SentStatusReport.ss']['SENDBOUNCED'] = 'Sending to the Following Recipients Bounced';
-$lang['en_US']['Newsletter_SentStatusReport.ss']['FAILEDBL'] = 'Sending to the Following Recipients Did Not Occur Because They Are BlackListed';
-$lang['en_US']['Newsletter_SentStatusReport.ss']['NEWSNEVERSENT'] = 'The Newsletter has Never Been Sent to Following Subscribers';
-$lang['en_US']['Newsletter_SentStatusReport.ss']['FN'] = 'Firstname';
-$lang['en_US']['Newsletter_SentStatusReport.ss']['SN'] = 'Surname';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['SENDFAIL'] = 'Sending to the Following Recipients Failed';
 $lang['en_US']['Newsletter_SentStatusReport.ss']['SENTOK'] = 'Sending to the Following Recipients was Successful';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['SN'] = 'Surname';
+$lang['en_US']['SubscribeForm']['PLURALNAME'] = array(
+	'Subscribe Forms',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['SubscribeForm']['SINGULARNAME'] = array(
+	'Subscribe Form',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['SubscribeForm']['db_AllNewsletters'] = array(
+	'AllNewsletters',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['SubscribeForm']['db_Subject'] = array(
+	'Subject',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['SubscribeForm']['db_Subscribe'] = array(
+	'Subscribe',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['SubscribeForm']['has_many_Newsletters'] = array(
+	'Newsletters',
+	50,
+	'Name of an object relation, mainly used for automatically generating forms'
+);
+$lang['en_US']['TemplateList']['NONE'] = 'None';
+$lang['en_US']['Unsubcribe']['SUBSCRIBEDTO'] = 'You are subscribed to the following lists:';
+$lang['en_US']['Unsubscribe']['EMAILADDR'] = 'Email address';
+$lang['en_US']['Unsubscribe']['NOMLSELECTED'] = 'You need to select at least one mailing list to unsubscribe from.';
+$lang['en_US']['Unsubscribe']['NOTSUBSCRIBED'] = 'I\'m sorry, but %s doesn\'t appear to be in any of our mailing lists.';
+$lang['en_US']['Unsubscribe']['REMOVESUCCESS'] = 'Thank you. %s will no longer receive the %s.';
+$lang['en_US']['Unsubscribe']['SHOWLISTS'] = 'Show lists';
+$lang['en_US']['Unsubscribe']['SUCCESS'] = 'Thank you. You have been removed from the selected groups';
+$lang['en_US']['Unsubscribe']['UNSUBSCRIBE'] = 'Unsubscribe';
+$lang['en_US']['UnsubscribeRecord']['PLURALNAME'] = array(
+	'Unsubscribe Records',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['UnsubscribeRecord']['SINGULARNAME'] = array(
+	'Unsubscribe Record',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
 
 ?>

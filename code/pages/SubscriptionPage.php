b834f521ffbcf936a3ad9c66603f1318ebb0e5bf
Normann Lou <normannlou@gmail.com>
2012-11-28 12:52:53 +1300

    MINOR: typo "RecievedCount" should be "ReceivedCount"

--- code/pages/SubscriptionPage.php
+++ code/pages/SubscriptionPage.php
@@ -58,7 +58,7 @@ class SubscriptionPage extends Page {
 			$exludes = array(
 				"BouncedCount",
 				"Blacklisted",
-				"RecievedCount",
+				"ReceivedCount",
 				"ValidateHash",
 				"ValidateHashExpired",
 			);

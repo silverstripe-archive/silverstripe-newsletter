86f39bc4613b8766211dfff73f19ff5d09ea4e9f
Ingo Schommer <ingo@silverstripe.com>
2012-11-28 11:08:54 +0100

    Fixed theme setting in travis configuration
    Was referencing 3.0 themes

--- tests/travis/_config.php
+++ tests/travis/_config.php
@@ -16,7 +16,7 @@ MySQLDatabase::set_connection_charset('utf8');
 
 // Set the current theme. More themes can be downloaded from
 // http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
+SSViewer::set_theme('blackcandy');
 
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if(class_exists('SiteTree')) SiteTree::enable_nested_urls();

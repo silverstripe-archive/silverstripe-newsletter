3880235077f0f209f821b0371eb2f888ec52b9df
Normann Lou <normannlou@gmail.com>
2012-12-05 00:57:55 +1300

    BUG: make CheckboxSetWithExtraField.js working with entwine

--- javascript/CheckboxSetWithExtraField.js
+++ javascript/CheckboxSetWithExtraField.js
@@ -1,32 +1,36 @@
-(function($) {
-	$(document).ready(function() {
-		
-		/**
-		 * @todo tableDnD is not live query compatibile.
-		 *
-		 * Needs to reapply events when tab is changed.
-		 */
-    	$("table.checkboxsetwithextrafield").tableDnD({
-			onDragClass: "DnD_whiledragging",
-			dragHandle: "dragHandle",
-			onDragStart: function(table, row) {
-				indicator = document.createElement('div');
-				indicator.innerHTML = 'Moving the row ...';
-				$(row).append(indicator);
+
+(function($){
+	$.entwine('ss', function($) {
+		$('table.checkboxsetwithextrafield').entwine({
+			UUID: null,
+			onmatch: function() {
+				this._super();
+				this.tableDnD({
+					onDragClass: "DnD_whiledragging",
+					dragHandle: ".dragHandle",
+					onDragStart: function(table, row) {
+						indicator = document.createElement('div');
+						$(indicator).html('Moving the row ...');
+						$(row).append($(indicator));
+					},
+					onDrop:function(table, row){
+						jQuery($(row).find(".dragHandle")[0]).empty();
+					}
+				});
 			},
-			onDrop:function(table, row){
-				jQuery($(row).children(".dragHandle")[0]).empty();
+			onunmatch: function() {
+				this._super();
 			}
 		});
-		
-		$("table.checkboxsetwithextrafield tbody tr").live("mouseover", function(){
-			jQuery($(this).children(".dragHandle")[0]).addClass('showDragHandle');
-		});
-		
-		$("table.checkboxsetwithextrafield tbody tr").live("mouseout", function(){
-			jQuery($(this).children(".dragHandle")[0]).removeClass('showDragHandle');
+		$('table.checkboxsetwithextrafield tbody tr').entwine({
+			onmouseover: function() {
+				jQuery($(this).children(".dragHandle")[0]).addClass('showDragHandle');
+			},
+			onmouseout: function() {
+				jQuery($(this).children(".dragHandle")[0]).removeClass('showDragHandle');
+			}
 		});
 	});
-})(jQuery);
+}(jQuery));
 
 

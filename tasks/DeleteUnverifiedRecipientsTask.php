297c607fe248fa6b02270ee8a6a65c4395aa37b1
Normann Lou <normannlou@gmail.com>
2012-12-12 18:30:21 +1300

    NEW: delete unverified recipients build task

---
+++ tasks/DeleteUnverifiedRecipientsTask.php
@@ -0,0 +1,35 @@
+<?php
+
+class DeleteUnverifiedRecipientsTask extends DailyTask{
+	static $trunk_length = 50;
+
+	public function process() {
+		$start = 0;
+		$total = 0;
+		while($deleted = $this->delete_trunk($start)){
+			$total = $total + $deleted;
+			$start = $start + self::$trunk_length;
+			if($deleted < self::$trunk_length) break;
+		}
+		echo "totally ".$total." recipients deleted";
+	}
+
+	public function delete_trunk($offset){
+		set_time_limit(18000);
+		ini_set('memory_limit','512M');
+
+		$objects = DataList::create('Recipient')
+			->where("\"Recipient\".\"Verified\" = 0 AND \"Recipient\".\"Created\" < NOW() - INTERVAL 2 DAY")
+			->limit(self::$trunk_length, $offset);
+
+		$count = $objects->count();
+		if($count){
+			foreach($objects as $object){
+				if($object->canDelete()) {
+					$object->delete();
+				}
+			}
+		}
+		return $count;
+	}
+}
